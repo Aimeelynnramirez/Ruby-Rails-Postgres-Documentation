d649981a0064129d6aca6906141351ec6aee3118
unknown <AB-WB@windowlight1222.2522>
2018-04-12 17:30:11 -0400

    test

--- src/scripts/components/Home.js
+++ src/scripts/components/Home.js
@@ -6,9 +6,10 @@ class Home extends React.Component {
 
         return (
             <div>
-                <h2>Homepage template</h2>
+                <h2>Homepage Tester</h2>
+                <img src="https://cdn-images-1.medium.com/max/1000/1*10rDJAvtCHTZz-NdIMpGLQ.jpeg" alt="wordpress banner"/>
                 <h1>{pageData.title.rendered}</h1>
-
+?
                 <div dangerouslySetInnerHTML={{__html: pageData.excerpt.rendered}} />
                 <div>{pageData.acf.text}</div>
             </div>
