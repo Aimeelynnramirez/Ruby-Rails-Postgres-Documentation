48da85350938c4997b059f654747ae9ff125ec8a
Aimee <aimeelynnramirez@gmail.com>
2018-04-14 19:22:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 
-
- <?php echo '<p>Hello World</p>'; ?> 
+#!/usr/bin/php
+<html>
+  <head>
+    <title>Hello, World!</title>
+  </head>
+  <body>
+    <p><?php echo "Hello, PHP World!"; ?></p>
+  </body>
+</html>
